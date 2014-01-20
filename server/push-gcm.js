c5bb32532e23179b4dd64e21274a586d7d5f33dd
MySmallfish <mysmallfish@gmail.com>
2014-01-20 10:43:05 +0200

    GCM Push Working

---
+++ server/push-gcm.js
@@ -0,0 +1,25 @@
+﻿var gcm = require('node-gcm');
+var message = new gcm.Message();
+
+//API Server Key
+var sender = new gcm.Sender('AIzaSyCNXXiM5PFWO6zTV3yGcNhUkI7cf364zME');
+var registrationIds = [];
+
+// Value the payload data to send...
+message.addData('message', process.argv[2]);
+message.addData('title', process.argv[3]);
+message.addData('msgcnt', process.argv[4]); // Shows up in the notification in the status bar
+message.addData('soundname', 'beep.wav'); //Sound to play upon notification receipt - put in the www folder in app
+//message.collapseKey = 'demo';
+//message.delayWhileIdle = true; //Default is false
+message.timeToLive = 3000;// Duration in seconds to hold in GCM and retry before timing out. Default 4 weeks (2,419,200 seconds) if not specified.
+
+// At least one reg id required
+registrationIds.push('APA91bEpTuw7L_O4NLP78JUOVTDfjCEcr5DlIsim4pJ1tE8caER7L4Paz6-jxafuPjrrIEDuRjmgpoNiD7jPzRquN7H9sF0CpZCDdi50ogM0GUdGDw_nekd-NXvlflOmJ6AXRG6hb4guF-rqaJBDLX3qp5uZo_nVLw');
+
+/**
+ * Parameters: message-literal, registrationIds-array, No. of retries, callback-function
+ */
+sender.send(message, registrationIds, 4, function (result) {
+    console.log(result);
+});
---
+++ server/send.bat
@@ -0,0 +1,1 @@
+node push-gcm.js "Hello! i'm ANDROID", "PG demo APP", 5
